85bcfc00eb41a93b511c5b31355a4d0f98fb7935
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-11-20 20:02:39 -0700

    added original code from default typeDefs

--- server/schemas/typeDefsWIP.js
+++ server/schemas/typeDefsWIP.js
@@ -28,3 +28,38 @@ const typeDefs = gql`
 `;
 
 module.exports = typeDefs;
+
+
+
+
+// original typeDefs below:
+
+//   type Profile {
+//     _id: ID
+//     name: String
+//     email: String
+//     password: String
+//     locations: [String]!
+//   }
+
+//   type Auth {
+//     token: ID!
+//     profile: Profile
+//   }
+
+//   type Query {
+//     profiles: [Profile]!
+//     profile(profileId: ID!): Profile
+//     # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
+//     me: Profile
+//   }
+
+//   type Mutation {
+//     addProfile(name: String!, email: String!, password: String!): Auth
+//     login(email: String!, password: String!): Auth
+
+//     addLocation(profileId: ID!, location: String!): Profile
+//     removeProfile: Profile
+//     removeLocation(location: String!): Profile
+//   }
+// `;

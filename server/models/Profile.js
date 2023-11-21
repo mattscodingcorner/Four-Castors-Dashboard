fda65f462c204f6b311379e141f9db8a7b948bbb
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-11-20 20:22:47 -0700

    added locations to profile model

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -21,7 +21,10 @@ const profileSchema = new Schema({
     required: true,
     minlength: 5,
   },
-  locations: [locationSchema],
+  locations: {
+    type: String,
+    trim: true,
+  }
 });
 
 // set up pre-save middleware to create password

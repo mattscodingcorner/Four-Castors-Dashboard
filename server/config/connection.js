c2cc5e511c09d0f845038db415b81440b94a86a2
unknown <matt.j.velazquez@gmail.com>
2023-11-30 12:03:07 -0700

    heroku / mongo deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fourcasters');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fourcasters.locations');
 
 module.exports = mongoose.connection;
